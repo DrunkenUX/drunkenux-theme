891c9a9b29dcf63593a761d5f3cd68c468e37764
Michael Fienen <fienen@gmail.com>
2021-02-01 22:41:11 -0600

    Just tossing some comments in

--- functions.php
+++ functions.php
@@ -1,1 +1,20 @@
 <?php
+/**
+ * drunkenux-theme functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * @link https://developer.wordpress.org/plugins/
+ *
+ * @package WordPress
+ * @subpackage drunkenux-theme
+ * @since Drunken UX Theme 2.0
+ */
