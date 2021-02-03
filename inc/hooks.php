8c6edcea75fd139996b11d8036f8509564965445
Michael Fienen <fienen@gmail.com>
2021-02-02 23:08:31 -0600

    Add post thumbnails and remove some head tags

--- inc/hooks.php
+++ inc/hooks.php
@@ -9,3 +9,11 @@ add_action( 'init', __NAMESPACE__ . 'action_register_template_podcast', 100 );
 
 // Filters
 add_filter( 'the_content', __NAMESPACE__ . 'filter_add_advertiser' );
+
+// Theme Support
+add_theme_support( 'post-thumbnails' );
+
+// Remove actions
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
