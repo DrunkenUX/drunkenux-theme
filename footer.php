4efcaafbefc6be2cc941aec1a88f14a2e9f043b0
Michael Fienen <fienen@gmail.com>
2021-02-02 20:58:29 -0600

    Scaffold the basic header/footer

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template footer file
+ *
+ * This is the template that displays everything before the closing </body>.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Drunken UX
+ * @subpackage drunkenux-theme
+ * @since Drunken UX Theme 2.0
+ */
+?>
+      </main>
+    </div><!-- #primary.content-area -->
+  </div><!-- #content.site-content -->
+</div><!-- #page.site -->
+
+<footer></footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template header file
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Drunken UX
+ * @subpackage drunkenux-theme
+ * @since Drunken UX Theme 2.0
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+<header></header>
+
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
+
+	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+
+	<div id="content" class="site-content">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
