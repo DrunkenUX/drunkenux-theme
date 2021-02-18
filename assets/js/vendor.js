7f71406cb1aad30a978cd65f6e521df0a4c09860
Michael Fienen <fienen@gmail.com>
2021-02-17 22:22:25 -0600

    Add gulp-include for package resolution

---
+++ assets/js/vendor.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _plyr = _interopRequireDefault(require("plyr"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var player = new _plyr.default('#player');
