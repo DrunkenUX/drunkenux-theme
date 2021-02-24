9e80d8663396df780cfa4990e4b2871fdf322835
Michael Fienen <fienen@gmail.com>
2021-02-23 22:39:10 -0600

    Hook menu registration

--- inc/hooks.php
+++ inc/hooks.php
@@ -22,6 +22,7 @@ add_action( 'wp_body_open', __NAMESPACE__ . '\action_add_gtm_body' );
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_scripts' );
 add_action( 'wp_head', __NAMESPACE__ . '\action_add_gtm_head' );
 add_action( 'init', __NAMESPACE__ . '\register_cpt_advertiser' );
+add_action( 'init', __NAMESPACE__ . '\action_register_menus' );
 add_action( 'init', __NAMESPACE__ . '\action_register_template_podcast', 100 );
 
 // ***** FILTERS *****
