ad96ab094bc235536f641960b7a1c7fae588edd2
Michael Fienen <fienen@gmail.com>
2021-02-26 10:20:38 -0600

    Move skip link up to top

--- header.php
+++ header.php
@@ -22,6 +22,8 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
+<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'drunkenux' ); ?></a>
+
 <header>
 	<h1>The Drunken UX Podcast</h1>
 
@@ -36,10 +38,6 @@
 </header>
 
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
-
-	<?php get_template_part( 'template-parts/header/site-header' ); ?>
-
 	<div id="content" class="site-content">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
