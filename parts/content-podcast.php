d5da5fde5686045655f3f7d55af2d2a6cb834d86
Michael Fienen <fienen@gmail.com>
2021-03-01 22:16:21 -0600

    #11: Initial stubs for single content parts

---
+++ parts/content-podcast.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template part for displaying podcast episodes
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Drunken UX
+ * @subpackage drunkenux-theme
+ * @since Drunken UX Theme 2.0
+ */
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <h1><?php the_title(); ?></h1>
+
+    <p>We wrote this on <time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>"><?php the_date( 'F j, Y' ); ?></time></p>
+
+    <?php the_content(); ?>
+
+    <div class="meta">
+        <?php the_tags( 'Tagged with:' ); ?>
+    </div><!-- .meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
---
+++ single.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Drunken UX
+ * @subpackage drunkenux-theme
+ * @since Drunken UX Theme 2.0
+ */
+
+get_header();
+
+/* Start the Loop */
+while ( have_posts() ) :
+	the_post();
+
+	get_template_part( 'parts/content', get_post_type() );
+
+endwhile; 
+
+get_footer();
