05a27444d143819e49afd777b1fcae78d82e7e87
Michael Fienen <fienen@gmail.com>
2021-03-04 22:04:47 -0600

    Basic grid layout for footer

--- footer.php
+++ footer.php
@@ -17,10 +17,13 @@
 </div><!-- #page.site -->
 
 <footer>
-  <small id="copyright">&copy; 2018-<?php echo date('Y'); ?> Fienen, LLC</small>
-  <small id="disclaimer">
-    The Drunken UX Podcast is a product of Fienen, LLC. The Drunken UX Podcast is a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for sites to earn advertising fees by advertising and linking to amazon.com.
-  </small>
+  <small class="copyright">&copy; 2018-<?php echo date('Y'); ?> Fienen, LLC</small>
+
+  <div class="widgets">
+    <small id="disclaimer">
+      The Drunken UX Podcast is a product of Fienen, LLC. The Drunken UX Podcast is a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for sites to earn advertising fees by advertising and linking to amazon.com.
+    </small>
+  </div>
 </footer>
 
 <?php wp_footer(); ?>
