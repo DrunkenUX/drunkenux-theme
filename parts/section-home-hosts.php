09616d2a2d136d07f0e93863615e8ded4bed0dc0
Michael Fienen <fienen@gmail.com>
2021-06-14 23:14:50 -0500

    #3: Set up partial for hosts

---
+++ parts/section-home-hosts.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part for displaying the latest blog posts on the homepage
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Drunken UX
+ * @subpackage drunkenux-theme
+ * @since Drunken UX Theme 2.0
+ */
+?>
+<section class="home-hosts">
+    <h2>Hosts</h2> 
+</section>
