92bf98e4b8f084dea07959a762a50d569c39a549
Michael Fienen <fienen@gmail.com>
2021-06-18 23:45:18 -0500

    Create test spec for footer

---
+++ cypress/integration/theme/footer.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+context('Template footer.php', () => {
+    before(() => {
+        cy.visit('https://drunkenux.local');
+    });
+
+    beforeEach(() => {
+        cy.get('footer').as('templateFooter');
+    });
+
+    describe('Footer Features', () => {
+        it('has a copyright statement', () => {
+            cy.get('@templateFooter')
+                .find('.copyright')
+                .should('contain', '2018-20')
+                .should('contain', 'Fienen, LLC');
+        });
+    });
+});
+ 
