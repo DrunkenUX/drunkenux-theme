725fd4d66b7e3e9aeaec6bf06436edba9ab2d810
Michael Fienen <fienen@gmail.com>
2021-06-22 23:23:05 -0500

    #3: Get initial user data

--- parts/section-home-hosts.php
+++ parts/section-home-hosts.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying the latest blog posts on the homepage
+ * Template part for displaying the host info on the homepage
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -8,7 +8,20 @@
  * @subpackage drunkenux-theme
  * @since Drunken UX Theme 2.0
  */
+
+ $host           = get_userdata( 1 );
+ $otherOtherHost = get_userdata( 2 );
 ?>
 <section class="home-hosts">
     <h2>Hosts</h2> 
+
+    <div class="hosts-wrapper">
+        <div class="host">
+            <?php echo $host->display_name; ?>
+        </div><!-- .host -->
+
+        <div class="host">
+            <?php echo $otherOtherHost->display_name; ?>
+        </div><!-- .host -->
+    </div><!-- .hosts-wrapper -->
 </section>
