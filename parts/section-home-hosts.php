fe5fb5d657b5673a3fcc20b8294498077b810ee5
Michael Fienen <fienen@gmail.com>
2021-10-10 00:01:05 -0500

    Fixes #3

--- parts/section-home-hosts.php
+++ parts/section-home-hosts.php
@@ -9,19 +9,37 @@
  * @since Drunken UX Theme 2.0
  */
 
- $host           = get_userdata( 1 );
- $otherOtherHost = get_userdata( 2 );
+ $hosts = get_users([
+    'meta_query' => [
+        'key' => 'host',
+        'value' => 'true'
+    ]
+ ]);
 ?>
 <section class="home-hosts">
     <h2>Hosts</h2> 
 
     <div class="hosts-wrapper">
+<?php 
+foreach($hosts as $host):
+    $host_meta = get_user_meta( $host->ID );
+    $host_name = $host->display_name;
+?>
         <div class="host">
-            <?php echo $host->display_name; ?>
-        </div><!-- .host -->
+            <?php echo wp_get_attachment_image($host_meta['headshot'][0]); ?>
 
-        <div class="host">
-            <?php echo $otherOtherHost->display_name; ?>
+            <h3><?php echo $host_name; ?></h3>
+
+            <?php echo apply_filters( 'the_content', $host_meta['description'][0] ); ?>
+    
+            <div class="host-icons">
+                <a href="https://github.com/<?php echo $host_meta['github_username'][0]; ?>" class="icon icon-github" title="<?php echo $host_name; ?> on GitHub"></a>
+                <a href="https://twitter.com/<?php echo $host_meta['twitter'][0]; ?>" class="icon icon-twitter" title="<?php echo $host_name; ?> on Twitter"></a>
+                <a href="<?php echo $host->user_url; ?>" class="icon icon-home" title="<?php echo $host_name; ?>'s other site"></a>
+            </div><!-- .host-icons -->
         </div><!-- .host -->
+<?php 
+endforeach; 
+?>
     </div><!-- .hosts-wrapper -->
 </section>
