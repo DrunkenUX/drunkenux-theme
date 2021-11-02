40bbe7678dce1c1137512c022ddff38f4f568594
Michael Fienen <fienen@gmail.com>
2021-11-01 22:11:57 -0500

    #40: Frame up basic page

---
+++ page.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying all single pages and posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-page
+ *
+ * @package Drunken UX
+ * @subpackage drunkenux-theme
+ * @since Drunken UX Theme 2.0
+ */
+
+$ep_banner  = get_field('cmb_thst_feature_post_img_id');
+
+get_header();
+
+/* Start the Loop */
+while ( have_posts() ) :
+	the_post(); 
+    
+    get_template_part( 'parts/section', 'banner' ); ?>
+
+<section>
+    <div class="meta">
+        <?php 
+        if(isset($ep_banner['description'])): 
+            echo '<p><strong>Image Credit:</strong> ' . $ep_banner['description'] . '</p>';
+        endif;
+        ?>
+    </div><!-- .meta -->
+</section>
+
+<?php 
+endwhile; // End of the loop.
+
+get_footer();
